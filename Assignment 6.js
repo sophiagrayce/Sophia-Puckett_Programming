0f57ede1d188c1e33e06403262fdad2496fcfee4
sophiapuckett <36088787+sophiagrayce@users.noreply.github.com>
2018-02-20 00:34:56 -0500

    Assignment6

---
+++ Assignment 6.js
@@ -0,0 +1,39 @@
+class Media {
+    constructor(name, duration) {
+      this.name = name;
+      this.duration = duration;
+    }
+}
+
+    class music extends media {
+        constructor(artist){
+          this.artist = artist;
+        }
+    }
+
+  
+    class podcast extends media
+
+      
+
+    class movie extends media{
+        constructor(rating){
+          this.rating = rating;
+        }
+    }
+  
+
+ // Method
+function next() { 
+next.staticMethod = function() {
+  return(this === next);
+};
+// Method
+function play() { }
+play.staticMethod = function() {
+  return(this === play);
+};
+
+Media.sort(function (a, b) {
+  return list;      
+}
