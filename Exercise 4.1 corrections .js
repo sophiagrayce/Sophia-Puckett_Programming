e9994335f405cfe95c40a43604f8aa079ab1db38
sophiapuckett <36088787+sophiagrayce@users.noreply.github.com>
2018-03-21 22:56:48 -0400

    exercise 4 corrections

---
+++ Exercise 4.1 corrections .js
@@ -0,0 +1,35 @@
+//Problem 1
+
+function average (arr)
+    for (int i=0; i < arr.length; i++)
+
+sum = (sum + sum)
+sum = sum /arr.length
+
+console.log((sum));
+
+
+//Problem 2
+var array1 = ['one', 'two', 'three'];
+var reversed = array1.reverse(); 
+
+console.log(array1);
+
+
+console.log(reversed);
+
+
+
+//Problem 4
+    var array = [ [1,2,3], [4,5,6] ];
+
+    array = array.reduce(function(a, b){
+         return a.concat(b);
+    }, []);
+
+
+//Problem 5
+
+var print = new Object();
+print.name = "Sophia";
+print.location = "Georgia";
