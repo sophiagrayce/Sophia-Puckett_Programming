86e06d1a4a2072bdedefb06b69b39d5d11c3e0ff
sophiapuckett <36088787+sophiagrayce@users.noreply.github.com>
2018-03-21 23:02:54 -0400

    Exercise 3.1 corrections

--- Exercise 3.1 part 2 .js
+++ Exercise 3.1 part 2 .js
@@ -1,26 +1,5 @@
-function sum_range (int_1, int_2);
-  loop;
-    if (int > int_1  < int_2) {
-       (int + int_1 + int_2) = sum;
-    console.log('sum')
-    }else{
-     (int_1 + int_2) = sum;
-    }
-    console.log ('sum')
 
 
 
 
-Example
 
-function sum_range (5, 9);
-  loop;
-    if (int > 5 < 9) {
-       (int + 5 + 9);
-       //add int (6,7,8,) to (5 + 9) = ('35')
-    console.log('35')
-    }else{
-    (1 + 2 == ('3');   
-       
-    
-    console.log ('3')
