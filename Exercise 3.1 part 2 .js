0fd43358be951c64ebbecef883e9b241e9f39561
sophiapuckett <36088787+sophiagrayce@users.noreply.github.com>
2018-03-21 23:31:13 -0400

    3.1

--- Exercise 3.1 part 2 .js
+++ Exercise 3.1 part 2 .js
@@ -1,5 +1,16 @@
+function sum_range(arr) {
+  var max = Math.max.apply(null, arr);
+  var min = Math.min.apply(null, arr);
+  var arr2 = [];
 
+  for (var i=min; i<=max; i++)
+  {
+    arr2.push(i);
+  }
 
+  return arr2.reduce(function(sum, item){sum+=item; return sum;},0);
+}
 
+sum_range([1, 4]);
 
 
