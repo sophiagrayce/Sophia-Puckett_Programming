3cf3729c29d2cec80a239657208612871da4aa40
sophiapuckett <36088787+sophiagrayce@users.noreply.github.com>
2018-03-24 23:45:30 -0400

    assignment 3

---
+++ Assignment 3 corrections 3.0.js
@@ -0,0 +1,49 @@
+function transpose_note(note, steps) {
+
+    if (note = 'a') 
+        'a'==0
+    if (note = 'b') 
+        'b'==1
+    if (note = 'c')
+        'c'==2
+    if (note = 'd') 
+        'd'==4
+    if (note = 'e') 
+        'e'==5
+    if (note = 'f') 
+        'f'==6
+    if (note = 'g') 
+        'g'==7
+        
+    
+    
+    
+    transpose = (note + steps) 
+    
+      if (transpose > 6) 
+        return (transpose - 7)
+    
+      if (note < 0);
+      transpose = (note - steps) 
+    
+      if (transpose < 0)
+        return (transpose + 7)
+    
+    }
+            
+    if (note = '0') 
+         0=='a'
+     if (note = '1') 
+         1=='b'
+     if (note = '2') 
+         2=='c'
+     if (note = '3') 
+         3=='d'
+     if (note = '4') 
+         4=='e'
+     if (note = '5')
+         5=='f'
+     if (note = '6')
+         6=='g'
+
+
