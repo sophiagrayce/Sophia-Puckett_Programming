01e5ff3ca088f729483b9fff8e9176d5b6f5e432
sophiapuckett <36088787+sophiagrayce@users.noreply.github.com>
2018-03-28 15:52:37 -0400

    assignment corrections

---
+++ assignment 4 problem 1 corrections.js
@@ -0,0 +1,40 @@
+
+//one
+function myset_add(data, new_value) {
+    var my_set = [0,1,2,3]; 
+    my_set.push('four')}
+    console.log( my_set ); 
+//two
+function myset_remove(data, remove_value) { 
+    var my_set = [0,1,2,3]; 
+    my_set.slice().forEach(
+        function myset_remove (data, remove_value)  {
+            if ( value === "1" ) {
+                
+                values.splice( index, 1 );
+
+//three
+// Return true if test_value is in the set, false otherwise
+function myset_has(data, test_value) {
+var my_set = [0,1,2,3]; 
+
+'my_set'.indexOf(1) >-1;
+
+'my_set'.indexOf('1') === -1; 
+    
+
+//four 
+function myset_size(data)
+var my_set = [0,1,2,3];  
+my_set.forEach(output);
+function output (index,item){
+    console.log(index, item);
+}
+
+
+//five
+function myset_forEach(data, callback_function) {}
+my_set.forEach(function(item) { 
+    var my_set = [0,1,2,3]; 
+    console.log(item);
+ }})
