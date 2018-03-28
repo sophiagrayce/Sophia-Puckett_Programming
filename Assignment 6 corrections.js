748e548418c29be6e7b6d4c56901a6e23d5fd682
sophiapuckett <36088787+sophiagrayce@users.noreply.github.com>
2018-03-28 00:17:40 -0400

    assignment corrections

---
+++ Assignment 6 corrections.js
@@ -0,0 +1,49 @@
+class Media {
+    constructor(name, duration) {
+      this.name = name;
+      this.duration = duration;
+    }
+}
+
+
+    class music extends media {
+        constructor(title, artist){
+          this.artist = artist;
+
+          t.toString(Song.title + "by" + Song.artist);
+        }
+    }
+
+  
+    class podcast extends media
+
+      
+
+    class movie extends media{
+        constructor(rating) {
+          this.rating = rating;
+
+          t.toString(Movie.title + "is rated" + Movie.rating);
+        }
+    }
+  
+    class playlist extends media{}
+    
+    let play = new playlist();
+    
+    play.add(new Song("song1");
+    console.log(play.current().toString());
+
+    play.add(new Film("film1");
+    play.add(new Song("song2");
+
+    console.log(play.current().toString());
+
+    play.next();
+   console.log(play.current().toString());
+
+    play.next();
+    console.log(play.current().toString());
+
+    play.next();
+    console.log(play.current().toString());
